85d8174b9d671eb4a71a5fc6e1370f2f78c3e603
Leonardo Rampanelli <leonardo.rampanelli@universo.univates.br>
2018-11-19 22:32:15 -0200

    rfid e display funcionando

--- doc/leitor-rfid/leitor.py
+++ doc/leitor-rfid/leitor.py
@@ -5,6 +5,14 @@ import time
 import RPi.GPIO as GPIO
 import MFRC522
 import sqlite3
+
+#importa as bibliotecas para trabalhar com o display
+import I2C_LCD_driver
+import socket
+import fcntl
+import struct
+
+lcdi2c = I2C_LCD_driver.lcd()
  
 # UID dos cartões que possuem acesso liberado.
 # 57:97:F4:DA:EE  - tag do cartao do prof anderson
@@ -18,6 +26,9 @@ try:
     LeitorRFID = MFRC522.MFRC522()
  
     print('Aproxime seu cartão RFID')
+    lcdi2c.lcd_display_string("Apro Cartao RFID", 1,0)
+    lcdi2c.lcd_display_string("Ou Dig Seu Cod", 2,0)
+
  
     while True:
         # Verifica se existe uma tag próxima do módulo.
@@ -25,14 +36,18 @@ try:
  
         if status == LeitorRFID.MI_OK:
             print('Cartão detectado!')
- 
+
             # Efetua leitura do UID do cartão.
             status, uid = LeitorRFID.MFRC522_Anticoll()
  
             if status == LeitorRFID.MI_OK:
                 uid = ':'.join(['%X' % x for x in uid])
                 print('UID do cartão: %s' % uid)
-                
+		lcdi2c.lcd_clear()
+		lcdi2c.lcd_display_string("ID do cartao:", 1,0)
+		lcdi2c.lcd_display_string("%s" % uid, 2,0)	
+                time.sleep(3)
+
                 conn = sqlite3.connect('Ponto.db')
                 cursor =  conn.cursor();
                 
@@ -48,10 +63,21 @@ try:
                     validade_pessoa = result[0][3]
                     print('Acesso Liberado!')
                     print("ola " + nome_pessoa)
+
+		    lcdi2c.lcd_clear()
+	            lcdi2c.lcd_display_string("Acesso Liberado", 1,0)
+		    lcdi2c.lcd_display_string("Ola " + nome_pessoa, 2,0)
+	            time.sleep(4)
                 else:
                     print('Acesso Negado!')
- 
+		    lcdi2c.lcd_clear()
+	            lcdi2c.lcd_display_string("Acesso Negado", 1,0)
+		    time.sleep(4); 
+
                 print('\nAproxime seu cartão RFID')
+		lcdi2c.lcd_clear()
+		lcdi2c.lcd_display_string("Aproxime Seu", 1,0)
+		lcdi2c.lcd_display_string("Cartao RFID", 2,0)
  
         time.sleep(.25)
 except KeyboardInterrupt:
