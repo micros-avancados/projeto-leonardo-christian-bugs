2a8822ebbd5aeffa47d2716fece71405b3b827a9
Leonardo Rampanelli <leonardo.rampanelli@universo.univates.br>
2018-12-10 21:55:16 -0200

    versão final

--- doc/leitor-rfid/leitor.py
+++ doc/leitor-rfid/leitor.py
@@ -16,13 +16,6 @@ import socket
 import fcntl
 import struct
 
-def check_host(host='165.227.9.145'):
-    a=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    a.settimeout(.5)
-    b=a.connect_ex((host, 80))
-    retorno=copy(b)
-    return b
-
 
 if __name__ == '__main__':
 
@@ -34,11 +27,6 @@ if __name__ == '__main__':
         # Inicia o módulo RC522.
         LeitorRFID = MFRC522.MFRC522()
        
-        if check_host()
-           print('tem internet')
-        else
-           print('não tem internet')
-     
         print('Aproxime seu cartão RFID')
         #lcdi2c.lcd_display_string("Apro Cartao RFID", 1,0)
         #lcdi2c.lcd_display_string("Ou Dig Seu Cod", 2,0)
@@ -69,41 +57,69 @@ if __name__ == '__main__':
                     lcdi2c.lcd_display_string("%s" % uid, 2,0)	
                     time.sleep(3)
                     
-                    conn_postgres = psycopg2.connect(host='165.227.9.145', dbname='ponto', 
-                    user='ponto', password='ponto')
-                    cursor_postgres = conn_postgres.cursor()
-
                     conn = sqlite3.connect('Ponto.db')
                     cursor =  conn.cursor();
-                    
-                    #cursor.execute("""select * from pessoas where rfid = ? and validade >= date('now'""", (uid,));
-                    
-                    cursor_postgres.execute("select * from pessoas where rfid = %s and validade >= date('now')", (uid,))
-                    
-                    #result = cursor.fetchall()
-                    result = cursor_postgres.fetchall()
-                     
-                    # Se o cartão está liberado exibe mensagem de boas vindas.
-                    if result:
-                        id_pessoa = result[0][0]
-                        nome_pessoa = result[0][1]
-                        rfid_pessoa = result[0][2]
-                        validade_pessoa = result[0][3]
-                        print('Ponto Registrado!')
-                        print("Ola " + nome_pessoa)
-                        
-                        cursor_postgres.execute("insert into marcacoes (ref_pessoa,data_hora) values ( %s, now() )",(id_pessoa,))
-                        conn_postgres.commit()
 
-                        lcdi2c.lcd_clear()
-                        lcdi2c.lcd_display_string("Ponto Registrado", 1,0)
-                        lcdi2c.lcd_display_string("Ola " + nome_pessoa, 2,0)
-                        time.sleep(4)
+
+                    host='165.227.9.145'
+                    a=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                    a.settimeout(.5)
+
+                    if a.connect_ex((host,80)):
+                       print('Sem conexao com a internet')
+                       cursor.execute("insert into marcacao (uid,data_hora) values (?, datetime('now') )",( uid,))
+                       conn.commit()
+
+
                     else:
-                        print('Usuario nao encontrado!')
-                        lcdi2c.lcd_clear()
-                        lcdi2c.lcd_display_string("Usuario nao enc", 1,0)
-                        time.sleep(4); 
+                       conn_postgres = psycopg2.connect(host='165.227.9.145', dbname='ponto',
+                       user='ponto', password='ponto')
+                       cursor_postgres = conn_postgres.cursor()
+
+                       print('Conectado a Internet')
+                       
+                       cursor.execute("""select * from marcacao""")
+                       marcacoes_antigas = cursor.fetchall()
+                       
+                       if marcacoes_antigas:
+                          linhas = len(marcacoes_antigas)
+                          contador = 0
+                          print('Inserindo Marcacoes Antigas')
+                          while(linhas > contador):
+                               cursor_postgres.execute("insert into marcacoes (ref_pessoa,data_hora) values ( (select id from pessoas where rfid = %s), %s )",(marcacoes_antigas[contador][0],marcacoes_antigas[contador][1]))
+                               conn_postgres.commit()
+                               contador = contador + 1                       
+                     
+                          cursor.execute("delete from marcacao")
+                          conn.commit()
+ 
+                           
+                       cursor_postgres.execute("select * from pessoas where rfid = %s and validade >= date('now')", (uid,))
+                    
+                       #result = cursor.fetchall()
+                       result = cursor_postgres.fetchall()
+                     
+                       # Se o cartão está liberado exibe mensagem de boas vindas.
+                       if result:
+                           id_pessoa = result[0][0]
+                           nome_pessoa = result[0][1]
+                           rfid_pessoa = result[0][2]
+                           validade_pessoa = result[0][3]
+                           print('Ponto Registrado!')
+                           print("Ola " + nome_pessoa)
+                        
+                           cursor_postgres.execute("insert into marcacoes (ref_pessoa,data_hora) values ( %s, now() )",(id_pessoa,))
+                           conn_postgres.commit()
+
+                           lcdi2c.lcd_clear()
+                           lcdi2c.lcd_display_string("Ponto Registrado", 1,0)
+                           lcdi2c.lcd_display_string("Ola " + nome_pessoa, 2,0)
+                           time.sleep(4)
+                       else:
+                           print('Usuario nao encontrado!')
+                           lcdi2c.lcd_clear()
+                           lcdi2c.lcd_display_string("Usuario nao enc", 1,0)
+                           time.sleep(4); 
                     
                     
                     print('\nAproxime seu cartão RFID')
